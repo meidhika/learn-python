39e4e8a91a788dfab83d4bf303276b7e591e3ad0
Meidhika Nawa Sapta <meidhikanawa@gmail.com>
2025-08-27 13:51:57 +0700

    tuples and sets

---
+++ tuples-sets.py
@@ -0,0 +1,18 @@
+#list
+data_list = [1,2,3,4,5]
+print(data_list)
+
+#Tuples (datanya tidak dapat dirubah)
+data_tuples = (1,2,3,4,5)
+print(data_tuples)
+print(data_tuples[1])
+
+#data tuples tidak dapat dirubah
+# data_tuples[1] = 10
+# data_tuples.append(10)
+
+
+#sets
+data_sets = {1,2,3,4,5}
+print(data_sets)
+print(data_sets[1])
