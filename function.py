6422f1e04c09f636f304fbd0af3c3127e8e626c9
Meidhika Nawa Sapta <meidhikanawa@gmail.com>
2025-08-28 13:22:31 +0700

    function

---
+++ function.py
@@ -0,0 +1,17 @@
+#Membuat Fungsi / FUnction
+import os
+
+os.system('cls')
+def hello_world():
+    print("Hello World")
+    print("Apa Kabar Dunia")
+    print("Selamat Pagi")
+
+hello_world()
+hello_world()
+
+def fungsi(nama):
+    #pemanggilan fungsi harus setelah dibuat
+    print(f"Hello {nama}")
+
+fungsi("dhika")
