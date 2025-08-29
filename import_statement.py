77437ed464c2fbf2999261c5af7afdd87a342035
Meidhika Nawa Sapta <meidhikanawa@gmail.com>
2025-08-29 15:36:10 +0700

    about import

---
+++ import_statement.py
@@ -0,0 +1,25 @@
+# import
+
+#fungsinya adalah untuk mengambil program lain dari external .py
+
+# 1. menyambung program dari external
+import import_program_print
+
+# 2. import dengan data
+import import_variabel
+
+#3. import dengan fungsi
+import import_fungsi
+
+
+
+
+
+print (import_variabel.data)
+import_program_print
+
+
+hasil = import_fungsi.tambah(4,5)
+print (hasil)
+
+
---
+++ import_variabel.py
@@ -0,0 +1,1 @@
+data = "Meidhika Nawa Sapta"
