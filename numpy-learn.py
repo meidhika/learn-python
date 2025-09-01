5944f3f8415859f69ddf1b9fd7e455120ddaa6ce
Meidhika Nawa Sapta <meidhikanawa@gmail.com>
2025-09-01 14:34:35 +0700

    numpy learn

---
+++ numpy-learn.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+list_a = [1,2,3,4,5]
+vector_a = np.array([1,2,3,4,5])
+
+
+print(f"list_a = {list_a}")
+print(f"vector_a = {vector_a}")
+print(f"vector_a pangkat 2= {vector_a**2}")
+print(f"vector_a kali 5 = {vector_a*5}")
+
+
+matrix_b = np.array([(1,2,3),(4,5,6),(7,8,9)])
+print(f"matrix_b = {matrix_b}")
+print(f"matrix b^2 = {matrix_b**2}")
+
+zeros_c = np.zeros((2,2))
+print(f"zeros_c = {zeros_c}")
+
+ones_d = np.ones((2,2))
+print(f"ones_d = {ones_d}")
+
